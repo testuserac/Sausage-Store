a2964d69cc5f60c2be38212c95e7f0109bbaee67
std-012-029 <d.v.saprykin@yandex.ru>
2023-01-27 15:49:58 +0300

    Changed DB to PostgreSQL

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -6,7 +6,7 @@ sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-back
 sudo rm -f /home/jarservice/sausage-store.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}/sausage-store-saprykin-daniil-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-sudo cp ./sausage-store-${VERSION}.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
+sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
 sudo systemctl restart sausage-store-backend
